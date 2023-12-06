c06ac7216171de72df02e0af8020c05f08e75e03
hsiddhant2000 <97123570+hsiddhant2000@users.noreply.github.com>
2023-12-06 15:23:13 +0530

    Update DeflectionOfBeam.py

--- DeflectionOfBeam.py
+++ DeflectionOfBeam.py
@@ -1,17 +1,21 @@
+#Defelection of a simply supported beam at a particular point provided by the user
+#Created on 29 September,2023
+#Last modified on 6 December,2023
 import math
 from sympy import*
 
-H = float(input("Enter the length of beam:"))
-M = float(input("Enter the Bending moment applied on beam:"))
+BeamHeight = float(input("Enter the length of beam:"))
+BM = float(input("Enter the Bending moment applied on beam:"))
 E = float(input("Enter the Youngs modulus of beam:"))
 I = float(input("Enter the Moment of Inertia:"))
 x = float(input("Enter the distance where deflection is going to happen:")) 
+pi = 3.14
 
-A = (-4*M*H*H)/(E*I*3.14*3.14*3.14)
+A = (-4*BM*BeamHeight**2)/(E*I*pi**3)
 
-y = A * math.sin((3.14*x)/H)
+Deflectiondistance = A * math.sin((pi*x)/BeamHeight)
 
-print("The deflection of beam at " ,x, "distance is:",y)
+print("The deflection of beam at " ,x, "distance is:",Deflectiondistance)
 
 
 
