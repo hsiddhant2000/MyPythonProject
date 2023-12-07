9db1db9ffd0e93c8bf122a6467bc36b55ddc7c78
hsiddhant2000 <97123570+hsiddhant2000@users.noreply.github.com>
2023-12-07 21:05:12 +0530

    Delete Numpy arrays/NumpyArraycreation6.py

--- Numpy arrays/NumpyArraycreation6.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-# for loop for printing 100 numbers between 0 and 1 and arranging them in an array
-for i in np.arange(0,1,0.01):
-    a = np.array(np.arange(0,1,0.01))
-#printing maximum number    
-print(max(a))
-#replacing the highest number with 99 and printing the final array
-a[a == max(a)] = 99
-print(a)
