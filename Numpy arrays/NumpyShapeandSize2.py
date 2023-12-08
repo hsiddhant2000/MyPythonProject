9840a5620148b81739b2f1c9203e3207d567b2fe
hsiddhant2000 <97123570+hsiddhant2000@users.noreply.github.com>
2023-12-08 18:30:01 +0530

    Delete Numpy arrays/NumpyShapeandSize2.py

--- Numpy arrays/NumpyShapeandSize2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-arr = np.array([[[1,2,3,4,6],[5,6,7,8,0],[9,10,11,12,9]]])
-#ndim is used to denote the number of dimensions of an array
-print(arr.ndim)
-#size is used to denote the number of elements of an array
-print(arr.size)
-#shape returns the shape of an array as a tuple 
-print(arr.shape)
